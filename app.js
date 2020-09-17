23c62f9e6f04b2863e1e2893ffb9d82459db69e4
Sovannara <sovannara@Sovannaras-MacBook-Pro.local>
2020-09-17 11:37:00 +0700

    Finished moving to pug template

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const path = require('path');
-const express = require("express");
-const bodyParser = require("body-parser");
-const rootDir = require('./util/path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-})
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
 
 app.listen(3000);
